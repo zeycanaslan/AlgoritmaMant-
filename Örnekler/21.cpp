08e5d1f1192e3fc9bd949d3346cd05a0da6230a9
Zeycan <131456231+zeycanaslan@users.noreply.github.com>
2023-04-25 02:11:30 +0300

    ÇARPIM TABLOSU AMA BASİTT OLAN
    ÇARPIM TABLOSU AMA BASİTT OLAN

---
+++ Örnekler/21.cpp
@@ -0,0 +1,16 @@
+//ÇARPIM TABLOSU AMA BASİTT OLAN
+ 
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	int i, j;
+
+	for (i = 0; i <= 10; i++) {
+		for (j = 0; j <= 10; j++) {
+			cout << "i*j=" << i * j << "	 ";
+		}
+		cout << endl;
+	}
+}
