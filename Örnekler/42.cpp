33927933340c6af0ada1aeeacbe5a6d679d7d879
Zeycan <131456231+zeycanaslan@users.noreply.github.com>
2023-04-25 04:12:30 +0300

    Bölme operatorü kullanılmadan bölüm bulma
    Bölme operatorü kullanılmadan bölüm bulma

---
+++ Örnekler/42.cpp
@@ -0,0 +1,8 @@
+/*Kendisine parametre olarak aldığı 2 pozitif tam sayıdan birincisi ikincisine bölündüğünde elde edilecek bölümü bularak geriye döndürecek
+bir C++ fonksiyonu yazınız.
+
+Bölme (/) ve modülüs (%) operatörlerinin kullanımına izin yoktur.
+Bölmenin tekrarlı bir çıkarma işlemi olduğunu hatırlayınız.
+
+Fonksiyonunuzu main içerisinden çağırarak doğru çalışıp çalışmadığını test ediniz.
+*/
